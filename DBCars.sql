6fed8705ce723ea2a4e79665fb01736fcdd72684
Inna <avramenkoinna6@gmail.com>
2023-05-30 22:15:25 +0300

    update DBCars

--- DBCars.sql
+++ DBCars.sql
@@ -1,1 +1,20 @@
-CREATE DATABASE `DBCars` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */
+CREATE DATABASE DBCars;
+
+CREATE TABLE Cars 
+(
+  id int NOT NULL AUTO_INCREMENT,
+  brand varchar(20) NOT NULL,
+  model varchar(20) NOT NULL,
+  engine_capacity float NOT NULL,
+  price int NOT NULL,
+  max_speed int NOT NULL,
+  PRIMARY KEY (id)
+ );
+  
+ INSERT INTO Cars
+ VALUES 
+ (1,'Opel','Astra',1.5,250000,180),
+ (2,'Audi','Q3',1.9,700000,220),
+ (3,'Toyota','RAW4',2,250000,180);
+ 
+SELECT * FROM Cars;

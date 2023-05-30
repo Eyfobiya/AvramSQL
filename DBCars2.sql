e8cd1128d056db9c6c16442c3fcf29761ef6785c
Inna Avramenko <88592610+Eyfobiya@users.noreply.github.com>
2023-05-30 18:40:41 +0300

    Add files via upload

---
+++ DBCars2.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE DBCars;
+
+CREATE TABLE Cars 
+(
+  id int NOT NULL AUTO_INCREMENT,
+  brand varchar(20) NOT NULL,
+  model varchar(20) NOT NULL,
+  engine_capacity float NOT NULL,
+  price int NOT NULL,
+  max_speed int NOT NULL,
+  PRIMARY KEY (id)
+ );
+  
+ INSERT INTO Cars
+ VALUES (1,'Opel','Astra',1.5,250000,180),
+ (2,'Audi','Q3',1.9,700000,220),
+ (3,'Toyota','RAW4',2,250000,180);
+ 
+SELECT * FROM Cars;
